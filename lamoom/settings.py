21b815ff8165469154362c841f2ffcdf4c71be47
Kate Yanchenko <kate@lamoom.com>
2025-02-12 17:58:04 -0800

    Updated deps

--- lamoom/settings.py
+++ lamoom/settings.py
@@ -27,8 +27,7 @@ SAFE_GAP_PER_MSG: int = os.environ.get("LAMOOM_SAFE_GAP_PER_MSG", 4)
 DEFAULT_ENCODING = "cl100k_base"
 
 USE_API_SERVICE = parse_bool(os.environ.get("LAMOOM_USE_API_SERVICE", True))
-LAMOOM_API_URI = os.environ.get("LAMOOM_API_URI", "https://api.lamoom.com")
-
+LAMOOM_API_URI = os.environ.get("LAMOOM_API_URI", os.environ.get("FLOW_PROMPT_API_URI", "https://api.lamoom.com"))
 CACHE_PROMPT_FOR_EACH_SECONDS = int(
     os.environ.get("LAMOOM_CACHE_PROMPT_FOR_EACH_SECONDS", 5 * 60)
 )  # 5 minutes by default
@@ -40,7 +39,7 @@ PIPE_PROMPTS = {}
 
 @dataclass
 class Secrets:
-    API_TOKEN: str = field(default_factory=lambda: os.getenv("LAMOOM_API_TOKEN"))
+    API_TOKEN: str = field(default_factory=lambda: os.getenv("LAMOOM_API_TOKEN", os.getenv("FLOW_PROMPT_API_TOKEN")))
     OPENAI_API_KEY: str = field(default_factory=lambda: os.getenv("OPENAI_API_KEY"))
     CLAUDE_API_KEY: str = field(default_factory=lambda: os.getenv("CLAUDE_API_KEY"))
     GEMINI_API_KEY: str = field(default_factory=lambda: os.getenv("GEMINI_API_KEY"))
